04957c42f1808cef4d8814e3f5bf62e9cf07a665
BilalElDeeb <BE2201234@tkh.edu.eg>
2025-04-12 17:00:56 +0200

    Added Door size Functionality
    Add the ability to change the door size dynamicall

--- Assets/Scripts/Room Scripts/Door Positions Manager.cs
+++ Assets/Scripts/Room Scripts/Door Positions Manager.cs
@@ -10,6 +10,7 @@ public class DoorPositionsManager : MonoBehaviour
     public Tilemap floorTilemap;
     public TileBase wallTile;
     public TileBase floorTile;
+    public int doorSize;
     public int roomHeight;
     public int roomWidth;
     public DoorPositions doorPositions;
@@ -17,29 +18,38 @@ public class DoorPositionsManager : MonoBehaviour
     List<Vector2Int> FetchDoorPostions(DoorPositions doorPositions)
     {
         List<Vector2Int> doorPositionsList = new List<Vector2Int>();
+        int halfDoorSize = doorSize / 2;
         
         if (doorPositions.hasFlag(DoorPositions.North))
         {
-            doorPositionsList.Add(new Vector2Int(roomWidth/2, roomHeight - 1));
-            doorPositionsList.Add(new Vector2Int((roomWidth/2) - 1, roomHeight - 1));
+            for (int i = (roomWidth/2) - halfDoorSize; i < (roomWidth/2) + halfDoorSize; i++)
+            {
+                doorPositionsList.Add(new Vector2Int(i, roomHeight - 1));
+            }
         }
         
-        if (doorPositions.hasFlag(DoorPositions.South))
+        if (doorPositions.hasFlag(DoorPositions.North))
         {
-            doorPositionsList.Add(new Vector2Int(roomWidth/2, 0));
-            doorPositionsList.Add(new Vector2Int((roomWidth/2) - 1, 0));
+            for (int i = (roomWidth/2) - halfDoorSize; i < (roomWidth/2) + halfDoorSize; i++)
+            {
+                doorPositionsList.Add(new Vector2Int(i, 0));
+            }
         }
         
         if (doorPositions.hasFlag(DoorPositions.East))
         {
-            doorPositionsList.Add(new Vector2Int(roomWidth - 1, roomHeight/2));
-            doorPositionsList.Add(new Vector2Int(roomWidth - 1,(roomHeight/2) - 1));
+            for (int i = (roomHeight/2) - halfDoorSize; i < (roomHeight/2) + halfDoorSize; i++)
+            {
+                doorPositionsList.Add(new Vector2Int(roomWidth - 1, i));
+            }
         }
         
         if (doorPositions.hasFlag(DoorPositions.West))
         {
-            doorPositionsList.Add(new Vector2Int(0, roomHeight/2));
-            doorPositionsList.Add(new Vector2Int(0, (roomHeight/2) - 1));
+            for (int i = (roomHeight/2) - halfDoorSize; i < (roomHeight/2) + halfDoorSize; i++)
+            {
+                doorPositionsList.Add(new Vector2Int(0, i));
+            }
         }
         
         return doorPositionsList;
