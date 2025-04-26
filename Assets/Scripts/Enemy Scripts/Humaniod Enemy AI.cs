5a7589d84b5732f7c0e434cf93c31c1b12ad2b90
BilalElDeeb <BE2201234@tkh.edu.eg>
2025-04-27 01:32:14 +0300

    Basic Skeletal animators done
    Basic Skeletal animators done as well as animations controller and the overrider of said controller

--- Assets/Scripts/Enemy Scripts/Humaniod Enemy AI.cs
+++ Assets/Scripts/Enemy Scripts/Humaniod Enemy AI.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Vector3 = System.Numerics.Vector3;
 
 public class HumaniodEnemyAI : EnemyAI
 {
@@ -11,25 +10,57 @@ public class HumaniodEnemyAI : EnemyAI
     public BulletItem bulletItem;
     public float fireRate;
     protected float attackCoolDown;
+    public AnimationClip attackAnimationClip;
+    public AnimationClip dyingAnimationClip;
+    public AnimationClip hurtAnimationClip;
+    public AnimationClip idleAnimationClip;
+    public AnimationClip runningAttackAnimationClip;
+    public AnimationClip runningAnimationClip;
+    public AnimationClip walkingAnimationClip;
+    protected Vector3 lastPosition;
+    
     
     // Start is called before the first frame update
     void Start()
     {
+        SetAnimations();
         attackCoolDown = fireRate;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (lastPosition != null)
+        {
+            animator.SetFloat("Speed", Vector3.Distance(transform.position, lastPosition) / Time.fixedDeltaTime);
+        }
         attackCoolDown = attackCoolDown - Time.fixedDeltaTime;
+        
+        enemyMovementBehaviour.EnemyMove(this, player.transform.position);
 
-        if (attackCoolDown < 0)
+        
+
+        if (attackCoolDown <= 0)
         {
             enemyAttackBehaviour.EnemyAttack(bulletItem, (player.transform.position - transform.position), bulletSpawnLocation.position, false, bulletPrefab);
             attackCoolDown = fireRate;
+            animator.SetTrigger("Attack");
         }
         
-        enemyMovementBehaviour.EnemyMove(this, player.transform.position);
+    }
+
+    void SetAnimations()
+    {
+        AnimatorOverrideController animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
         
+        animatorOverrideController ["Idle Placeholder"] = idleAnimationClip;
+        animatorOverrideController ["Walking Placeholder"] = walkingAnimationClip;
+        animatorOverrideController ["Running Placeholder"] = runningAnimationClip;
+        animatorOverrideController["Attack Placeholder"] = attackAnimationClip;
+        animatorOverrideController["Dying Placeholder"] = dyingAnimationClip;
+        animatorOverrideController["Hurt Placeholder"] = hurtAnimationClip;
+        animatorOverrideController["Running Attack Placeholder"] = runningAttackAnimationClip;
+        
+        animator.runtimeAnimatorController = animatorOverrideController;
     }
 }
