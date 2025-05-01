29b4c86a392ae612b8d2b684f3e4437c89e585e4
BilalElDeeb <BE2201234@tkh.edu.eg>
2025-05-01 07:09:06 +0300

    created initialize function so that the item pickup prefab may adopt its intended sprite

--- Assets/Scripts/Inventory Scripts/Item Pickup.cs
+++ Assets/Scripts/Inventory Scripts/Item Pickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,7 +8,20 @@ public class ItemPickup : MonoBehaviour
 {
     public ItemSO item;
     public int amount;
+    
 
+    private void Awake()
+    {
+        initialize(this.item, amount);
+    }
+
+    void initialize(ItemSO item, int amount)
+    {
+        this.item = item;
+        this.GetComponent<SpriteRenderer>().sprite = item.itemImage;
+        this.transform.localScale = new Vector3(item.itemScale, item.itemScale, 1);
+        this.amount = amount;
+    }
     void OnTriggerEnter2D(Collider2D target)
     {
         if (target.tag == "Player")
